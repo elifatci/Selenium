748951db7b9a4d751ac5f8b8386ef4c508b51cfa
Elif Atçı <eelif0600@gmail.com>
2023-11-15 23:26:05 +0300

    151123

---
+++ src/main/java/Tekrarlar/T5.java
@@ -0,0 +1,53 @@
+package Tekrarlar;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class T5 {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "kurulumDosyalari/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://testotomasyonu.com");
+
+        List<WebElement> brands = driver.findElements(By.xpath("//a[@class='brandbox']"));
+
+        Thread.sleep(2000);
+        int expectedResult = 6;
+        int actualResult = brands.size();
+        if (expectedResult == actualResult) {
+            System.out.println("Test is PASSED");
+
+        } else {
+            System.out.println("Test is FAİLED");
+        }
+
+        boolean selected = false;
+
+        for (WebElement each : brands) {
+            if (each.isSelected()) {
+                selected = true;
+                break;
+            }
+        }
+        if (selected == true) {
+            System.out.println("SelectedTest PASSED");
+        } else {
+            System.out.println("SelectedTest FAİLED");
+        }
+
+
+        driver.quit();
+
+
+    }
+}
